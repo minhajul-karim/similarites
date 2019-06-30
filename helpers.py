fc9fa7ad086c69172293f932c8bbf7101e6b9abe
Minhajul Karim <minhajul.kaarim@gmail.com>
2019-06-30 20:20:03 +0600

    Add lines() in helpers.py. Modified index.html accoding to application.py

--- helpers.py
+++ helpers.py
@@ -1,8 +1,12 @@
 def lines(a, b):
     """Return lines in both a and b"""
+    # Split strings into lists
+    list1 = a.split('\n')
+    list2 = b.split('\n')
 
-    # TODO
-    return []
+    # Remove duplicates from both lists and find the common elements
+    list_of_common_elements = list(set(list1) & set(list2))
+    return list_of_common_elements
 
 
 def sentences(a, b):
